d35d5beab01da8c972648afa0af0dbdb8d490499
Marshall Ford <inbox@marshallford.me>
2024-04-03 19:35:51 -0500

    improves run timeout behavior and run error messages (#25)

---
+++ pkg/ansible/navigator_artifact.go
@@ -0,0 +1,48 @@
+package ansible
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type PlaybookArtifact struct {
+	Status string   `json:"status"`
+	Stdout []string `json:"stdout"`
+}
+
+func getPlaybookArtifact(dir string) (*PlaybookArtifact, error) {
+	path := filepath.Join(dir, playbookArtifactFilename)
+
+	contents, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read playbook artifact, %w", err)
+	}
+
+	var artifact PlaybookArtifact
+	if err = json.Unmarshal(contents, &artifact); err != nil {
+		return nil, fmt.Errorf("failed to parse playbook artifact, %w", err)
+	}
+
+	return &artifact, nil
+}
+
+func GetStatusFromPlaybookArtifact(dir string) (string, error) {
+	artifact, err := getPlaybookArtifact(dir)
+	if err != nil {
+		return "", err
+	}
+
+	return artifact.Status, nil
+}
+
+func GetStdoutFromPlaybookArtifact(dir string) (string, error) {
+	artifact, err := getPlaybookArtifact(dir)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Join(artifact.Stdout, "\n"), nil
+}
