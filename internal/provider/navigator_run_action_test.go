49bdc69d7ed9bd02103d4cf31f71b1e4f85f06d4
Marshall Ford <inbox@marshallford.me>
2025-11-20 18:17:54 -0600

    fix test

--- internal/provider/navigator_run_action_test.go
+++ internal/provider/navigator_run_action_test.go
@@ -4,7 +4,9 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
 func TestAccNavigatorRunAction_basic(t *testing.T) {
@@ -13,6 +15,9 @@ func TestAccNavigatorRunAction_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(version.Must(version.NewVersion("1.14.0"))), // TODO replace with tfversion.Version1_14_0 when new plugin-testing version is released
+		},
 		// TODO use TestCheckProgressMessageContains when new plugin-testing version is released
 		Steps: []resource.TestStep{
 			{
